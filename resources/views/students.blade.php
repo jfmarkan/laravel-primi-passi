77596b13a0952450bbfa46a6a88d621c177daec1
Juan Francisco Markan <jfmarkan@gmail.com>
2023-07-18 19:14:51 +0200

    played along with slack and laravel

---
+++ resources/views/students.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boolean Classe #98</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <h1>BOOLEAN Classe #98</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>This is my first Laravel try. What I intended to do is to create 3 files to list a relational array in every single file wht the names of the Students, the Teachers, and all the other members of Boolean that work together to drive us through the process of becoming Professional Full-Stack web developers.</p>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown button
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="{{ route('students') }}">Students</a></li>
+                <li><a class="dropdown-item" href="{{ route('teachers') }}">Teachers</a></li>
+                <li><a class="dropdown-item" href="{{ route('advisors') }}">Advisors</a></li>
+            </ul>
+        </div>
+        <div class="row d-flex justify-content-between mb-4">
+        @foreach ($students as $student)
+            <div class="col-3 mt-4">
+                <div class="card">
+                    <img src="{{ $student['img'] }}" class="card-img-top" alt="{{ $student['name'] }}'s Picture" style="max-height:304px; object-fit:cover;">
+                    <div class="card-body">
+                        <p class="card-text text-center">{{ $student['name'] }} {{ $student['surname'] }}</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+        </div>
+    </div>
+    
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,281 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return view('home');
 });
+
+Route::get('/students-list', function () {
+    $data = [
+        'students' => [
+            [
+                'name' => 'Alessio',
+                'surname' => 'Di nardo',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V602JWJW-36409e18db0b-512',
+            ],
+            [
+                'name' => 'Alessio',
+                'surname' => 'Giovinetti',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V6A17XMG-314fc10f699f-512',
+            ],
+            [
+                'name' => 'Alex',
+                'surname' => 'Timoncini',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V63FVAF5-84d142d5fc85-512',
+            ],
+            [
+                'name' => 'Cosimo Damiano',
+                'surname' => 'Colucci',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U0504H6KK9P-807fb249928f-512',
+            ],
+            [
+                'name' => 'Cuono',
+                'surname' => 'Maione',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V8PFGXMJ-4746b4a2bd9d-512',
+            ],
+            [
+                'name' => 'Daniel',
+                'surname' => 'Marella',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04VB6C45CL-b0b243847546-512',
+            ],
+            [
+                'name' => 'Dario',
+                'surname' => 'La Seta',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V2T0M1HT-4a4322621cb1-512',
+            ],
+            [
+                'name' => 'Denise',
+                'surname' => 'Dichiara',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V8B73A68-8d4d952aae2d-512',
+            ],
+            [
+                'name' => 'Eligio',
+                'surname' => 'Biasi',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04UZ6DLM8E-858c87a88f9a-512',
+            ],
+            [
+                'name' => 'Emanuela',
+                'surname' => 'Pau',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U050FFF9L2C-de325728fa15-512',
+            ],
+            [
+                'name' => 'Emanuele',
+                'surname' => 'Farelli',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V5R5HR7C-g13aaa988112-512',
+            ],
+            [
+                'name' => 'Ericson',
+                'surname' => 'Manalo',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V5QWAG7M-1b5291432e0e-512',
+            ],
+            [
+                'name' => 'Federico',
+                'surname' => 'Rosso',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V2QRUBU5-g7328c7bd311-512',
+            ],
+            [
+                'name' => 'Flavio',
+                'surname' => 'Vitale',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U0504GJJR6D-g48ce7a84b2c-512',
+            ],
+            [
+                'name' => 'Francesco',
+                'surname' => 'Bifero',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U050FJ1B0LQ-15cb7f8f1e4d-512',
+            ],
+            [
+                'name' => 'Francesco',
+                'surname' => 'Ruggeri',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U050FMCSD96-03b5c530aa8b-512',
+            ],
+            [
+                'name' => 'Francesco',
+                'surname' => 'Troiano',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V6662WF5-d58fe297a605-512',
+            ],
+            [
+                'name' => 'Francesco',
+                'surname' => 'Valentino',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U0504U09UL9-131773ffba2a-512',
+            ],
+            [
+                'name' => 'Gabriele',
+                'surname' => 'Grossi',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04UZC425T8-e1462e560482-512',
+            ],
+            [
+                'name' => 'Gabriele',
+                'surname' => 'Majocchi',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04UZB80JHL-688697285d9e-512',
+            ],
+            [
+                'name' => 'Giuseppe',
+                'surname' => 'La Maestra',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V3E2FX5K-2a8f7609cfd1-512',
+            ],
+            [
+                'name' => 'Jacopo',
+                'surname' => 'Grippiotti',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U050G0LAB1N-07f7d66c667a-512',
+            ],
+            [
+                'name' => 'Jean Patrick',
+                'surname' => 'Nana Hell',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V2R4265B-g47956cbdcbb-512',
+            ],
+            [
+                'name' => 'Juan Francisco',
+                'surname' => 'Markán',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04UREKT5QX-943309550372-512',
+            ],
+            [
+                'name' => 'Leonardo',
+                'surname' => 'Pascale',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04664NHK8A-0da5a5d3ebe8-512',
+            ],
+            [
+                'name' => 'Lisa',
+                'surname' => 'Kozumi Cipolla',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V60Q06G2-f4660c247f61-512',
+            ],
+            [
+                'name' => 'Louis',
+                'surname' => 'Assane Faye',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U050H04FT24-g540a2d62e7d-512',
+            ],
+            [
+                'name' => 'Luca',
+                'surname' => 'Mazzola',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04URSENGBH-e860fa706717-512',
+            ],
+            [
+                'name' => 'Manuel',
+                'surname' => 'Camaioni',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U0504ESREUR-6b87b6164a38-512',
+            ],
+            [
+                'name' => 'Mario',
+                'surname' => 'Santoro',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U0504DP02Q1-8d6c262892a3-512',
+            ],
+            [
+                'name' => 'Matteo',
+                'surname' => 'Didonè',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V65ZRDHR-d4ebe4900d77-512',
+            ],
+            [
+                'name' => 'Mohamed',
+                'surname' => 'Jebali',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04UZ5DJ8J2-e6cc425f8763-512',
+            ],
+            [
+                'name' => 'Simona',
+                'surname' => 'Cartolina',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U0504U2987K-15f32fa17a18-512',
+            ],
+            [
+                'name' => 'Simone',
+                'surname' => 'Scoca',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04US00BX1D-db481581761b-512',
+            ],
+            [
+                'name' => 'Toni',
+                'surname' => 'Virgilio',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U0504TT3J5P-aec598f825c1-512',
+            ],
+            [
+                'name' => 'Vito',
+                'surname' => 'Pizzulli',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04V2VADETF-ed6f44a77db8-512',
+            ],
+        ]
+    ];
+
+    return view('students', $data);
+})->name('students');
+
+
+Route::get('/teachers-list', function () {
+    $data = [
+        'teachers' => [
+            [
+                'name' => 'Antonio',
+                'surname' => 'Sagoleo',
+                'role' => 'Teacher Assistant',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04SRMW57L5-f69897790ce2-512',
+            ],[
+                'name' => 'Riccardo',
+                'surname' => 'Petricca',
+                'role' => 'Teacher',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U021P1AKF71-54d4eb983323-512',
+            ],[
+                'name' => 'Samuel',
+                'surname' => 'Panicucci',
+                'role' => 'Teacher Assistant',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U04P3BZSS15-7390d1d984cb-512',
+            ],[
+                'name' => 'Stefano',
+                'surname' => 'Cappellini',
+                'role' => 'Lead Teacher Assistant',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U02HYRKL8E5-ca77aa4493a4-512',
+            ],[
+                'name' => 'Thomas',
+                'surname' => 'de Luca',
+                'role' => 'Teacher Assistant',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U01MH7KH1DZ-08f6f6a78cbc-512',
+            ],
+        ]
+    ];
+
+    return view('teachers', $data);
+})->name('teachers');
+
+Route::get('/advisors-list', function () {
+    $data = [
+        'advisors' => [
+            [
+                'name' => 'Elisabetta',
+                'surname' => 'Dahò',
+                'role' => 'Teacher Coordinator',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U014JPTQT4Y-5cf3a5f89015-512'
+            ],[
+                'name' => 'Eugenia',
+                'surname' => 'Faiola',
+                'role' => 'Team Boolean',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U02JZ1TSZ0U-4054f2f77576-512'
+            ],[
+                'name' => 'Filippo',
+                'surname' => 'Mangione',
+                'role' => 'Head of Teaching',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U02525XDF0Q-4c1c4ddddd8d-512'
+            ],[
+                'name' => 'Margherita',
+                'surname' => 'Ghidini',
+                'role' => 'Senior Career Advisor',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-UMX6FL3NY-3128fac4bf82-512'
+            ],[
+                'name' => 'Marilena',
+                'surname' => 'Costa',
+                'role' => 'Team Boolean',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U0234R0RM9A-3561306d844e-512'
+            ],[
+                'name' => 'Marta',
+                'surname' => 'Seminara',
+                'role' => 'Team Boolean',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U019JFGJRGC-dad2f89178d5-512'
+            ],[
+                'name' => 'Simone',
+                'surname' => 'Rubici',
+                'role' => 'Career Advisor',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U01N2LFCT6C-e20d06413214-512'
+            ],[
+                'name' => 'Valeria',
+                'surname' => 'Marta',
+                'role' => 'Admissions and Onboarding',
+                'img' => 'https://ca.slack-edge.com/T91QPE3BP-U01A09U9L94-60401f05265d-512'
+            ],
+        ]
+    ];
+
+    return view('advisors', $data);
+})->name('advisors');
